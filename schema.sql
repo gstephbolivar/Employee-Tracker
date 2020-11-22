814dabe63b3d1e4d78b45aa3b52285022d1ae296
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-11-21 19:19:01 -0500

    Created basic database with 3 tables

---
+++ schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS	employeetracker_db;
+
+CREATE DATABASE employeetracker_db;
+
+USE employeetracker_db;
+
+CREATE TABLE department(
+id INTEGER NOT NULL AUTO_INCREMENT,
+name VARCHAR(30) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+id INTEGER NOT NULL AUTO_INCREMENT,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL(10, 2),
+department_id INTEGER,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+id INTEGER NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INTEGER,
+manager_id INTEGER,
+PRIMARY KEY (id)
+);
