679c1427c8cd092acd44718371582fa68b36fe9b
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-11-23 15:21:58 -0500

    [WIP] addEmployee function

--- server.js
+++ server.js
@@ -95,13 +95,13 @@ function addDept() {
     });
 }
 
-// Function that creates an array of the departments to be called in addRole(); for user to choose 
+// Function that creates an array of the departments to be called in addRole(); for user to choose
 function deptChoice() {
-    let department= [];
+  let department = [];
   connection.query("SELECT name FROM department", (err, res) => {
     if (err) throw err;
-    for (let i = 0; i < res.length; i++){
-        department.push(res[i].name);
+    for (let i = 0; i < res.length; i++) {
+      department.push(res[i].name);
     }
   });
   return department;
@@ -122,30 +122,79 @@ function addRole() {
         message: "What is the salary for this position?",
       },
       {
-          type: "list",
-          name: "deptchoice",
-          message: "What department would you like to add this role to?",
-          choices: deptChoice(),
+        type: "list",
+        name: "deptchoice",
+        message: "What department would you like to add this role to?",
+        choices: deptChoice(),
       },
     ])
     .then((userInput) => {
       console.log(userInput);
-      connection.query("INSERT INTO role SET ?",
-      {
+      connection.query(
+        "INSERT INTO role SET ?",
+        {
           title: userInput.title,
           salary: userInput.salary,
           department_id: userInput.deptChoice,
-      },
-       (err, res) => {
+        },
+        (err, res) => {
           if (err) throw err;
-        // console.log("You did it!");
-      })
+          // console.log("You did it!");
+        }
+      );
       userOptions();
     });
 }
 
+function roleChoice() {
+    let role = [];
+    connection.query("SELECT title FROM role", (err, res) => {
+      if (err) throw err;
+      for (let i = 0; i < res.length; i++) {
+        role.push(res[i].title);
+      }
+    });
+    return role;
+  }
+
 // function to add a new employee
-function addEmployee() {}
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstname",
+        message: "What is the employees first name?",
+      },
+      {
+        type: "input",
+        name: "lastname",
+        message: "What is the employees last name?",
+      },
+      {
+        type: "list",
+        name: "rolechoice",
+        message: "What role does this employee have?",
+        choices: roleChoice(),
+      },
+      // {
+      //     insert manager id here
+      // },
+    ])
+    .then((userInput) => {
+      console.log(userInput);
+      connection.query("INSERT INTO employee SET ?",
+       {
+        first_name: userInput.firstname,
+        last_name: userInput.lastname,
+        role_id: userInput.rolechoice,
+      }),
+        (err, res) => {
+          if (err) throw err;
+        };
+      userOptions();
+    });
+}
 
 // Function to view all departments
 function viewAllDept() {
@@ -188,60 +237,3 @@ function exit() {
   connection.end();
 }
 
-// function addRole() {
-//   connection.query("SELECT * FROM department"),
-//     (err, res) => {
-//       if (err) throw err;
-//       inquirer
-//         .prompt([
-//           {
-//             type: "input",
-//             name: "title",
-//             message: "What is the role title?",
-//           },
-//           {
-//             type: "input",
-//             name: "salary",
-//             message: "What is the salary for this role?",
-//           },
-//           {
-//             type: "list",
-//             name: "deptId",
-//             message: "What is the department this role is in?",
-//             choices: function () {
-//               const choicesArray = [];
-//               for (let i = 0; i < res.length; i++) {
-//                 choicesArray.push(res[i].name);
-//               }
-//               return choicesArray;
-//             },
-//           },
-//         ])
-//         .then((userInput) => {
-//           console.log(userInput);
-//           let chosenItem;
-//           for (let i = 0; i < res.length; i++) {
-//             if (userInput.choice === res[i].name) {
-//               chosenItem = res[i];
-//             }
-//           }
-//           if (userInput.deptID === chosenItem.name) {
-//             connection.query(
-//               "INSERT INTO role SET ?",
-//               {
-//                 name: userInput.title,
-//                 salary: userInput.salary,
-//               },
-//               {
-//                 department_id: chosenItem.id,
-//               },
-//               (err, res) => {
-//                 if (err) throw err;
-//                 console.log(res.affectedRows);
-//                 userOptions();
-//               }
-//             );
-//           }
-//         });
-//     };
-//  }
